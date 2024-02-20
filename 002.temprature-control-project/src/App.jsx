282f0ff1cab91b349b9fdccd911ff87964b6bc70
banchet18 <banjarac6@gmail.com>
2024-02-20 23:45:33 +0530

    c

---
+++ 002.temprature-control-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const App = () => {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className="temperature-display">{temperatureValue}°C</div>
+      </div>
+      <div className="button-container">
+        <button onClick={() => setTemperatureValue(temperatureValue + 1)}>
+          +
+        </button>
+        <button>+</button>
+      </div>
+    </div>
+  );
+};
+export default App;
