6457714935d22b82f65bc1e817b777bc50425587
banchet18 <banjarac6@gmail.com>
2024-02-22 10:29:52 +0530

    c

--- react-movie-app/README.md
+++ react-movie-app/README.md
@@ -6,5 +6,13 @@ steps :
 3.cleaning the file from src and cleaning the UI
 4.using omdbi api for project
 5.register and active the api key
+6.import styled component in app.js
+7.installing styled component extension
+8.creating header
+9.inserting header in container
+10.importing "Josefin Sans" font from google fonts -> index.html then index.css
 
 new to learn :
+
+1. what is styled components and how to use them?
+2. importing fonts?
--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
+const container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: black;
+  color: white;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+
 function App() {
-  return <div>the indian</div>;
+  return (
+    <container>
+      <Header>React Movie App</Header>the indian
+    </container>
+  );
 }
 
 export default App;
